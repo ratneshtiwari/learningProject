313cd2d19357e3629f01d342aa458d241fa83a74
Ratnesh Tiwari <ratneshtanuj26@gmail.com>
2018-04-11 08:44:28 +0530

    update

---
+++ src/providers/touch.ts
@@ -0,0 +1,28 @@
+
+import { Injectable } from '@angular/core';
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio';
+
+/*
+Generated class for the TouchProvider provider.
+
+See https://angular.io/guide/dependency-injection for more info on providers
+and Angular DI.
+*/
+@Injectable()
+export class TouchProvider {
+
+  constructor(private faio: FingerprintAIO) {
+    console.log('Hello TouchProvider Provider');
+  }
+  verifyFinger() {
+    this.faio.show({
+      clientId: 'Fingerprint-Demo',
+      clientSecret: 'password', //Only necessary for Android
+      disableBackup: true,  //Only for Android(optional)
+      localizedFallbackTitle: 'Use Pin', //Only for iOS
+      localizedReason: 'Please authenticate' //Only for iOS
+    })
+      .then((result: any) => console.log(result))
+      .catch((error: any) => console.log(error));
+  }
+}
